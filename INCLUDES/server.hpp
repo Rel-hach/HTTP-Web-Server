ed952ae79603356e955e3def7093f4e4991918b9
Rel-hach <rachidelhachimi6@gmail.com>
2023-05-19 02:26:43 +0100

    feat: add server headerfile

---
+++ INCLUDES/server.hpp
@@ -0,0 +1,19 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <iostream>
+#include <sys/socket.h>
+
+
+class server
+{
+    private:
+        int     sockFd;
+
+    public:
+        // void    create_socket ( void );
+        // void    bind_socket( void );
+        // void    start_server( void );
+};
+
+#endif
