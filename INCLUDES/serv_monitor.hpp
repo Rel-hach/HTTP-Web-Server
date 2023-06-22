b3a7047f764c6ab3a055000aa297fbb3fe0e06ca
rel-hach <rachidelhachimi6@gmail.com>
2023-06-22 18:00:51 +0100

    add: some defines & useful methods

--- INCLUDES/serv_monitor.hpp
+++ INCLUDES/serv_monitor.hpp
@@ -7,6 +7,10 @@
 #include <map>
 #include <poll.h>
 
+#define EVENT_OCCURED _ALLFDS[i].revents
+#define LISTENING_SOCKET    checking_ifServerSocket(_ALLFDS[i].fd, listeners)
+#define CONNECTING_SOCKET   !checking_ifServerSocket(_ALLFDS[i].fd, listeners)
+
 
 class Server
 {
@@ -26,11 +30,13 @@ class   Monitor
         // methods :
 
             void    starting_theProcess ( std::vector<Server> servers );
-            int     getting_serverIndex();
-            void    accepting_newClient();
-            void    receiving_clientRequest();
+            bool    checking_ifServerSocket(int fd, std::vector<Listeningsock>& listener);
+            int     getting_serverIndex(int fd, std::vector<Listeningsock> listeners);
+            void    accepting_newClient( Server& server, Listeningsock& listener );
+            void    receiving_clientRequest( int& connFd, size_t& i, Server& server );
             void    sending_responseToClient();
-            void    writing_errorMessage();
+            void    writing_errorMessage( pollfd& fd, int i );
 };
+
 
 #endif
