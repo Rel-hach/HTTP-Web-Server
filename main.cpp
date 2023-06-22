2de2c7e2bd5a88646ac8d582210089824adb657d
rel-hach <rachidelhachimi6@gmail.com>
2023-06-22 22:57:12 +0100

    add: main function

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "INCLUDES/parse_config.hpp"
+#include "INCLUDES/ListeningSocket.hpp"
+#include "INCLUDES/serv_monitor.hpp"
+
+int main() 
+{
+    std::ifstream file("conf/default.toml");
+
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open the file." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    std::ostringstream bufferStream;
+    while (std::getline(file, line))
+        bufferStream << line << '\n';
+
+    std::string buffer = bufferStream.str();
+
+    std::vector<Server> servers;
+    splitByServer(buffer, servers);
+    parseDirectives(servers);
+    Monitor monitor;
+    monitor.starting_theProcess(servers);
+
+
+    return 0;
+}
