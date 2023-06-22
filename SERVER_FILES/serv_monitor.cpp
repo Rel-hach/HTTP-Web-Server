ee890f84063673fae8ff74b228cdfe6de4f3aa20
rel-hach <rachidelhachimi6@gmail.com>
2023-06-22 17:18:13 +0100

    using poll to wait for events & loop throu all fds

--- SERVER_FILES/serv_monitor.cpp
+++ SERVER_FILES/serv_monitor.cpp
@@ -14,7 +14,50 @@ void    Monitor::starting ( std::vector<Server> servers )
     }
 
     _numOfFds = _ALLFDS.size();
-    
+
+    while (true)
+    {
+        // using poll to wait for an event to come.
+        int ret = poll(&_ALLFDS[0], _numOfFds, -1);
+
+        if (ret < 0)
+        {   std::cerr << "Poll function has been failed " << std::endl;
+            break ; }
+
+        size_t i = 0;
+        for (i = 0; i <_numOfFds; i++)
+        {
+            if (IS_SERVER_FD && _ALLFDS[i].revents & POLLIN)
+            {
+                // getting server index ..
+                int index = getting_serverIndex();
+                // accepting the new client 
+                accepting_newClient();
+            }
+            else if (!IS_SERVER_FD && _ALLFDS[i].revents & POLLIN)
+            {
+                // getting server index 
+                int index = getting_serverIndex();
+                // receiving request from client
+                receiving_request(_ALLFDS[i].fd, i, servers[index]);
+            }
+            else if (!IS_SERVER_FD && _ALLFDS[i].revents & POLLOUT)
+            {
+                // socket is ready for writing
+                // sending request
+
+                // deleting fd from pollfds
+                // close fd
+                // delete request oject.
+            }
+            else
+            {
+                // there is an error.
+                // writing_error 
+            }
+        }
+    }
+
 }
 
 
