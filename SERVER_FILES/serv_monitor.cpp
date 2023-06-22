b045626764a59a6d78bb183183b381c88d53fcdc
rel-hach <rachidelhachimi6@gmail.com>
2023-06-22 17:00:31 +0100

    feat: create a listening socket to each server

--- SERVER_FILES/serv_monitor.cpp
+++ SERVER_FILES/serv_monitor.cpp
@@ -1,3 +1,20 @@
-#include "../INCLUDES/server.hpp"
-#include "../INCLUDES/request.hpp"
+#include "../INCLUDES/serv_monitor.hpp"
+
+
+
+void    Monitor::starting ( std::vector<Server> servers )
+{
+    std::vector<Listeningsock> listeners(servers.size());
+
+    for (int i = 0; i < servers.size(); i++)
+    {
+        int fd = listeners[i].creating_ListeningSockets(servers[i]);
+        fcntl(fd, F_SETFL, O_NONBLOCK);
+        _ALLFDS.push_back({fd, POLLIN, 0});
+    }
+
+    _numOfFds = _ALLFDS.size();
+    
+}
+
 
