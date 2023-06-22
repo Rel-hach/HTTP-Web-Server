6271263d19bad5069576ae71d9f06c668559b8b0
rel-hach <rachidelhachimi6@gmail.com>
2023-06-22 18:02:08 +0100

    feat : accepting client case & add it to pollfd.

--- SERVER_FILES/serv_monitor.cpp
+++ SERVER_FILES/serv_monitor.cpp
@@ -2,7 +2,7 @@
 
 
 
-void    Monitor::starting ( std::vector<Server> servers )
+void    Monitor::starting_theProcess ( std::vector<Server> servers )
 {
     std::vector<Listeningsock> listeners(servers.size());
 
@@ -27,21 +27,21 @@ void    Monitor::starting ( std::vector<Server> servers )
         size_t i = 0;
         for (i = 0; i <_numOfFds; i++)
         {
-            if (IS_SERVER_FD && _ALLFDS[i].revents & POLLIN)
+            if (LISTENING_SOCKET && EVENT_OCCURED & POLLIN)
             {
                 // getting server index ..
-                int index = getting_serverIndex();
+                int index = getting_serverIndex (_ALLFDS[i].fd, listeners);
                 // accepting the new client 
-                accepting_newClient();
+                accepting_newClient( servers[index], listeners[index]  );
             }
-            else if (!IS_SERVER_FD && _ALLFDS[i].revents & POLLIN)
+            else if (CONNECTING_SOCKET && EVENT_OCCURED & POLLIN)
             {
                 // getting server index 
                 int index = getting_serverIndex();
                 // receiving request from client
-                receiving_request(_ALLFDS[i].fd, i, servers[index]);
+                receiving_clientRequest();
             }
-            else if (!IS_SERVER_FD && _ALLFDS[i].revents & POLLOUT)
+            else if (CONNECTING_SOCKET && EVENT_OCCURED & POLLOUT)
             {
                 // socket is ready for writing
                 // sending request
@@ -59,5 +59,65 @@ void    Monitor::starting ( std::vector<Server> servers )
     }
 
 }
+
+
+bool     Monitor::checking_ifServerSocket(int fd, std::vector<Listeningsock>& listeners)
+{
+    for (size_t i = 0; i < listeners.size(); i++)
+    {
+        if (fd == listeners[i]._fd)
+            return (true);
+    }
+    return (false);   
+}
+
+
+
+int     Monitor::getting_serverIndex(int fd, std::vector<Listeningsock> listeners)
+{
+    for (size_t i = 0; i < listeners.size(); i++)
+    {
+        if (fd == listeners[i]._fd)
+            return (i);
+    }
+    return (0);
+}
+
+void    Monitor::accepting_newClient( Server& server, Listeningsock& listener )
+{
+    int connFd = accept(listener._fd, NULL, NULL);
+    if (connFd > 0)
+    {
+        _ALLFDS.push_back( {connFd, POLLIN, 0} );
+        fcntl(connFd, F_SETFL, O_NONBLOCK);
+        _numOfFds++;
+    }
+    else
+        close(connFd);
+}
+
+void    Monitor::receiving_clientRequest( int& connFd, size_t& i, Server& server )
+{
+
+}
+
+void    Monitor::sending_responseToClient()
+{
+
+}
+
+void    Monitor::writing_errorMessage( pollfd& fd, int i)
+{
+    if (fd.revents & POLLERR)
+        std::cerr << "POLLER: An error occurred on the file descriptor ";
+    else
+        std::cerr << "Error: An error occurred";
+    // closing & removing _ALLFDS[i] from pollfd
+    close (_ALLFDS[i].fd);
+    std::vector<pollfd>::iterator it_p = _ALLFDS.begin() + i;
+    _ALLFDS.erase(it_p);
+}
+
+
 
 
