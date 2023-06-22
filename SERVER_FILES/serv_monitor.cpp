438da90963c806e22955736a8b35081e07b27a0b
rel-hach <rachidelhachimi6@gmail.com>
2023-06-23 00:19:40 +0100

    add: strings error pages for testing.

--- SERVER_FILES/serv_monitor.cpp
+++ SERVER_FILES/serv_monitor.cpp
@@ -2,6 +2,51 @@
 
 #include "../INCLUDES/serv_monitor.hpp"
 
+// utils 
+
+
+std::string    getting_errorPage(int status)
+{
+    std::string Badreq = "<HTTP/1.1 400 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Bad Request : 400 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Bad Request</h1><p>Sorry, we were unable to process your request.</p></div></body></html>"; 
+    std::string unauthorized = "<HTTP/1.1 401 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Unauthorized : 401 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Unauthorized</h1><p>Sorry, you are not authorized.</p></div></body></html";
+    std::string not_found = "<HTTP/1.1 404 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Not Found : 404 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Not Found</h1><p>Sorry, not found.</p></div></body></html";
+    std::string method_notAllowd = "<HTTP/1.1 405 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Not Allowd : 405 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Not Allowed</h1><p>Sorry, method is not allowed.</p></div></body></html";
+    std::string tooLong = "<HTTP/1.1 414 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Too Long Uri : 414 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Too Long Uri</h1><p>Sorry, uri is too long.</p></div></body></html";
+    std::string httpVersionErr = "<HTTP/1.1 505 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Not Supported : 505 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Not Supported</h1><p>Sorry, version (HTTP) not supported.</p></div></body></html";
+    std::string timeOut = "<HTTP/1.1 504 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Time Out : 504 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Timeout</h1><p>Sorry, Gateway timeout.</p></div></body></html";
+    std::string internal_error = "<HTTP/1.1 500 \r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title> Internal server issue : 500 </title><style>body{font-family: Arial, sans-serif;background-color: #f7f7f7;}.container{margin: 50px auto;text-align: center;}h1{color: #d0021b;font-size: 36px;margin-bottom: 20px;}p{font-size: 18px;margin-bottom: 20px;}button{background-color: #d0021b;color: #fff;border: none;padding: 10px 20px;font-size: 18px;border-radius: 5px;cursor: pointer;}button:hover {background-color: #a30014;}</style></head><body><div class='container'><h1>Internal issue</h1><p>Sorry, there is an internal server issue.</p></div></body></html";
+    
+    switch (status)
+    {
+    case 400 :
+        return (Badreq);
+    
+    case 401 :
+        return (unauthorized);
+    
+    case 404 :
+        return (not_found);
+    
+    case 405 :
+        return (method_notAllowd);
+    
+    case 414 :
+        return (tooLong);
+    
+    case 500 :
+        return (internal_error);
+    
+    case 504 :
+        return (timeOut);
+    
+    case 505 :
+        return (httpVersionErr);
+    
+    default:
+        return (Badreq);
+    }
+}
+
 
 void    Monitor::starting_theProcess ( std::vector<Server> servers )
 {
@@ -29,20 +74,16 @@ void    Monitor::starting_theProcess ( std::vector<Server> servers )
         {
             if (LISTENING_SOCKET && EVENT_OCCURED & POLLIN)
             {
-                // getting server index ..
-                int index = getting_serverIndex (_ALLFDS[i].fd, listeners);
-                // accepting the new client 
-                if (accepting_newClient( servers[index], listeners[index] ))
+                int index = getting_serverIndex(_ALLFDS[i].fd, listeners);
+                if (accepting_newClient( _ALLFDS[i].fd ))
                 {
-                    request Request(_ALLFDS[i].fd, servers[index]);
+                    request Request(servers[index]);
                     _clients.insert(std::make_pair(_ALLFDS[i].fd, Request));
                 }
             }
 
             else if (CONNECTING_SOCKET && EVENT_OCCURED & POLLIN)
             {
-                // getting server index 
-                int index = getting_serverIndex(_ALLFDS[i].fd, listeners);
                 receiving_clientRequest(_ALLFDS[i].fd, i);
             }
 
@@ -58,11 +99,8 @@ void    Monitor::starting_theProcess ( std::vector<Server> servers )
                 // delete request oject.
             }
 
-            else
-            {
-                // there is an error.
-                // writing_error 
-            }
+            else if (EVENT_OCCURED & POLLERR)
+                writing_errorMessage(_ALLFDS[i], i);
         }
     }
 
@@ -94,15 +132,15 @@ int     Monitor::getting_serverIndex(int fd, std::vector<Listeningsock> listener
 
 // ------------------------------- [ACCEPTING]---------------
 
-bool    Monitor::accepting_newClient( Server& server, Listeningsock& listener )
+bool    Monitor::accepting_newClient( int listenFd)
 {
-    int connFd = accept(listener._fd, NULL, NULL);
+    int connFd = accept(listenFd, NULL, NULL);
     if (connFd > 0)
     {
         _ALLFDS.push_back( {connFd, POLLIN, 0} );
         fcntl(connFd, F_SETFL, O_NONBLOCK);
         _numOfFds++;
-        return (true)
+        return (true);
     }
     else
         close(connFd);
@@ -166,6 +204,9 @@ void    Monitor::receiving_clientRequest( int& connFd, size_t& i)
         {
             // handle GET case;
 
+            std::string getTest = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello, World! I AM THE GET METHOD</h1></body></html>";
+            _clients[connFd]._response = getTest;
+            _ALLFDS[i].events = POLLOUT;
             _ALLFDS[i].events = POLLOUT; 
         }
 
@@ -173,7 +214,6 @@ void    Monitor::receiving_clientRequest( int& connFd, size_t& i)
         {
             // handle Delete case;
             // _clients[connFd]._response = handling response.
-            _clients[connFd]._response = getTest;
             _ALLFDS[i].events = POLLOUT;
         }
     }
@@ -197,7 +237,7 @@ bool    Monitor::requestIsFullyReceived( request& request )
 // ---------------------- [SENDING] ----------
 
 
-void    Monitor::sending_responseToClient( int& fd, int& i)
+void    Monitor::sending_responseToClient( int& fd, size_t& i)
 {
     size_t size = _clients[fd]._response.size();
     if (send(fd, _clients[fd]._response.c_str(), size - 1, 0) == -1)
@@ -241,40 +281,4 @@ void    Monitor::writing_errorMessage( pollfd& fd, int i)
 }
 
 
-
-// utils 
-
-
-char*    getting_errorPage(int status)
-{
-    switch (status)
-    {
-    case 400 :
-        return (Badreq);
-    
-    case 401 :
-        return (unauthorized);
-    
-    case 404 :
-        return (not_found);
-    
-    case 405 :
-        return (method_notAllowd);
-    
-    case 414 :
-        return (tooLong);
-    
-    case 500 :
-        return (internal_error);
-    
-    case 504 :
-        return (timeOut);
-    
-    case 505 :
-        return (httpVersionErr);
-    
-    default:
-        return (Badreq);
-    }
-}
 
