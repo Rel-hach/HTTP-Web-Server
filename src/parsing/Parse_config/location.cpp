e133fed691c53bdc804164ed294c0216928ad9bd
fatimzehraa <elbouaazzaouitema@gmail.com>
2023-07-03 17:05:48 +0000

    delete construction messages

--- src/parsing/Parse_config/location.cpp
+++ src/parsing/Parse_config/location.cpp
@@ -2,21 +2,17 @@
 
 location::location()
 {
-	std::cout << "location constructor called" << std::endl;
 }
 
 location::location(const location &location)
 {
-	std::cout << "location copy constructor called" << std::endl;
 }
 
 location &location::operator=(const location &location)
 {
-	std::cout << "location assigned called" << std::endl;
 	return *this;
 }
 
 location::~location()
 {
-	std::cout << "location destructor called" << std::endl;
 }
