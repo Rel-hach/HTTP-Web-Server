333c9d36bb924d2c713418aa51a4ec22a2af8863
fatimzehraa <elbouaazzaouitema@gmail.com>
2023-07-03 17:06:58 +0000

    I'm only using this file as backup I'm gonna clear it later

--- src/parsing/REQUEST/delete_case.cpp
+++ src/parsing/REQUEST/delete_case.cpp
@@ -1,5 +1,30 @@
 
 
+void handel_key(std::ifstream file, server server, int what)
+{
+	std::string line;
+	std::string key;
+	std::stringstream ss;
+	std::cout << "\033[4;35mfound error_page\033[0m" << std::endl;
+	while (std::getline(file, line))
+	{
+		ss.str("");
+		ss.clear();
+		ss << line;
+		ss >> key;
+		if (line[0] == '#' || line.empty())
+			continue;
+		else if (key == "[[server.error_page]]" || key == "[[server]]" || key == "[[server.location]]")
+			break;
+//		if (what == 1)
+//			fill_server(server, line);
+		if (what == 2)
+			fill_location(server.locations.back(), line);
+		else if (what == 3)
+			fill_error_page(server.error_pages, line);
+	}
+}
+
 	try {
 		file.open(config_file);
 		while (std::getline(file, line)){
