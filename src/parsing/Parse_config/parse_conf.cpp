9bd0039e3b287b1cad519f55877266e95ab5c737
fatimzehraa <58591844+fatimzehraa@users.noreply.github.com>
2023-07-05 22:49:05 +0100

    impl: handle_key

--- src/parsing/Parse_config/parse_conf.cpp
+++ src/parsing/Parse_config/parse_conf.cpp
@@ -56,15 +56,30 @@ void fill_location(location &location, std::string line){
 //	key = line.substr(0, line.find('='));
 //	value = line.substr(line.find('=') + 1, line.length());
 	if (key == "uri")
+	{
 		location.uri = value;
+		return;
+	}
 	else if (key == "allow_methods")
+	{
 		location.allow_methods.push_back(value);
+		return;
+	}
 	else if (key == "cgi_path")
+	{
 		location.cgi_path = value;
+		return;
+	}
 	else if (key == "autoindex")
+	{
 		location.autoindex = value;
+		return;
+	}
 	else if (key == "return_code")
+	{
 		location.return_code = value;
+		return;
+	}
 	else
 		throw std::invalid_argument("Error: invalid location key");
 }
@@ -77,11 +92,41 @@ void fill_error_page(std::map<int , std::string> error_page, std::string line){
 	ss >> key;
 	ss >> eq;
 	ss >> value;
+//TODO: check if key is int
+//TODO: check if value is string
+//TODO: read until end of line for value
 	std::cout << "key = " << key << std::endl;
 	std::cout << "value = " << value << std::endl;
 	if (key < 400 || key > 599)
 		throw std::invalid_argument("Error: invalid error_page key");
 	error_page[key] = value;
+}
+
+std::string handle_key(std::ifstream& file, std::string old_key, server &server){
+
+	std::string line;
+	std::string key;
+	std::stringstream ss;
+	while (std::getline(file, line)){
+		ss.str("");
+		ss.clear();
+		ss << line;
+		ss >> key;
+		std::cout << "key in handle = " << key << std::endl;
+		if (line[0] == '#' || line.empty())
+			continue;
+		else if (key == "[[server.error_page]]" || key == "[[server]]" || key == "[[server.location]]")
+			return key;
+		//	break;
+		if (old_key == "[[server.error_page]]")
+			fill_error_page(server.error_pages, line);
+		else if (old_key == "[[server.location]]")
+			fill_location(server.locations.back(), line);
+		else
+			return key;
+//			fill_server(server, line);
+	}
+	return key;
 }
 
 std::vector<server> parse_server(std::string config_file){
@@ -95,7 +140,7 @@ std::vector<server> parse_server(std::string config_file){
 	try {
 		file.open(config_file);
 		while (std::getline(file, line)){
-			if (key != "[[server.location]]" && key != "[[server.error_page]]")
+			if (key != "[[server]]" && key != "[[server.location]]" && key != "[[server.error_page]]")
 			{
 				ss.str("");
 				ss.clear();
@@ -115,54 +160,54 @@ std::vector<server> parse_server(std::string config_file){
 						ss << line;
 						ss >> key;
 					}
-//					std::cout << "hnaaaaaaaaa line = " << line << std::endl;
-//					std::cout << "hnaaaaaaaaa key = " << key << std::endl;
 					std::cout << "key in try = " << key << std::endl;
 					if (line[0] == '#' || line.empty())
 						continue;
 					else{
 
-						if (key == "[[server]]")
-							break;
 						if (key == "[[server.error_page]]"){
-								std::cout << "\033[4;35mfound error_page\033[0m" << std::endl;
-								while (std::getline(file, line)){
-									ss.str("");
-									ss.clear();
-									ss << line;
-									ss >> key;
-									if (line[0] == '#' || line.empty())
-										continue;
-									else if (key == "[[server.error_page]]" || key == "[[server]]" || key == "[[server.location]]")
-										break;
-									fill_error_page(servers.back().error_pages, line);
-								}
+							std::cout << "\033[4;35mfound error_page\033[0m" << std::endl;
+							key = handle_key(file, key, servers.back());
+							//	while (std::getline(file, line)){
+							//		ss.str("");
+							//		ss.clear();
+							//		ss << line;
+							//		ss >> key;
+							//		if (line[0] == '#' || line.empty())
+							//			continue;
+							//		else if (key == "[[server.error_page]]" || key == "[[server]]" || key == "[[server.location]]")
+							//			break;
+							//		fill_error_page(servers.back().error_pages, line);
+							//	}
 							}
 
 						if (key == "[[server.location]]"){
 								std::cout << "\033[4;35mfound location\033[0m" << std::endl;
-								servers.back().locations.push_back(location());
-								while (std::getline(file, line)){
-									ss.str("");
-									ss.clear();
-									ss << line;
-									ss >> key;
-									if (line[0] == '#' || line.empty())
-										continue;
-									else if (key == "[[server.location]]" || key == "[[server]]" || key == "[[server.error_page]]")
-										break;
-									fill_location(servers.back().locations.back(), line);
-								}
+								key = handle_key(file, key, servers.back());
+							//	servers.back().locations.push_back(location());
+							//	while (std::getline(file, line)){
+							//		ss.str("");
+							//		ss.clear();
+							//		ss << line;
+							//		ss >> key;
+							//		if (line[0] == '#' || line.empty())
+							//			continue;
+							//		else if (key == "[[server.location]]" || key == "[[server]]" || key == "[[server.error_page]]")
+							//			break;
+							//		fill_location(servers.back().locations.back(), line);
+							//	}
 							}
 						if (key == "[[server.location]]" || key == "[[server.error_page]]")
 							continue;
+						if (key == "[[server]]")
+							break;
 						}
-//TODO: you need to read the line twice
 					std::cout << "line = " << line << std::endl;
 					fill_server(servers.back(), line);
 				}
 			}
 		}
+		file.close();
 	}
 
 	catch (const std::exception&) {
