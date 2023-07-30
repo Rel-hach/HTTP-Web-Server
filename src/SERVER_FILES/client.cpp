96db18fbf349fad07e89ce6995092a736b01c0cb
jalloulmohamed <medjalal1997@gmail.com>
2023-07-30 17:17:41 +0100

    test server with siege tools

--- src/SERVER_FILES/client.cpp
+++ src/SERVER_FILES/client.cpp
@@ -1,5 +1,6 @@
 #include "../../inc/client.hpp"
-
+#include <fstream>  
+#include <sstream> 
 client::client()
 {
 
@@ -18,6 +19,53 @@ client::client(int fd)
     this->firstbuff = false;
     this->server_name="";
     this->sendLenth = 0;
+    this->proccessing = false;
+   
+    //     std::ifstream file1("root/1.mp4", std::ios::binary);
+
+    //     if (!file1.is_open()) {
+    //     std::cerr << "Failed to open the file." << std::endl;
+    //     exit(0);
+    //     }
+
+    //     // Create a stringstream to store the video data
+    //     std::stringstream stream;
+    //     stream << file1.rdbuf();
+    //     file1.close();
+
+    //     // Get the video data as a string from the stringstream
+    //     std::string vidio = stream.str();
+
+    //    this->res = "HTTP/1.1 200 OK\r\n";
+    //     this->res += "Content-Type: video/mp4\r\n";
+    //     this->res += "Set-Cookie: Darkmode=true\r\n";
+    //     this->res += "Content-Length: " + std::to_string(vidio.length()) + "\r\n";
+    //     this->res += "Set-Cookie: sessionID=abc123; Path=/; Secure; HttpOnly\r\n\r\n";
+    //     // Concatenate the headers and video binary data
+    //     this->res += vidio;
+
+//    text
+    // std::ifstream file("root/index.html");
+    // std::string html;
+    // std::string line; 
+    // if (!file.is_open()) {
+    //     std::cerr << "Failed to open the file." << std::endl;
+    // } 
+    // if (file.is_open()) 
+    // {
+    //     while (std::getline(file, line))
+    //         html+= line;
+    //     file.close();
+    // }
+
+    // std::string headers = "HTTP/1.1 200 OK\r\n";
+    // headers += "Content-Type: text/html\r\n";
+    // headers += "Set-Cookie: Darkmode=true\r\n";
+    // headers += "Content-Length: " + std::to_string(html.length()) + "\r\n";
+    // headers += "Set-Cookie: sessionID=abc123; Path=/; Secure; HttpOnly\r\n\r\n";
+    // headers +=   html;
+
+    
 
 }
 
@@ -102,7 +150,6 @@ void client::setservr_name(char const *first_req)
         if(index1 != (int)std::string::npos && index2 != (int)std::string::npos )
         {
             this->server_name = buff.substr(index1 , index2 - index1);
-            
         }
     }
 }
